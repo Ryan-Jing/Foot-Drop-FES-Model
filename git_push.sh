76498d6afb9dffeb180e895a97f7e5b760ddb8e1
Ryan-Jing <ryanjing23@gmail.com>
2024-03-14 13:03:02 -0400

    Update from Google Docs

---
+++ git_push.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Change directory to your repository path
+cd /Users/ryanjing/MatLab_Projects/355_Project/Foot-Drop-FES-Model
+
+# Add the modified file to the staging area
+git add BME_355_Project_Model_Implementation.m
+
+# Commit the changes
+git commit -m "Update from Google Docs"
+
+# Push changes to the remote repository
+git push origin code-group-uploads

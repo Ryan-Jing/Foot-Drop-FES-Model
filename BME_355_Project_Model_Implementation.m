b5248a30a1c8f249d082047bdc639b08917457f0
Ryan-Jing <ryanjing23@gmail.com>
2024-03-13 22:45:59 -0400

    Testing pulling from google docs

--- BME_355_Project_Model_Implementation.m
+++ BME_355_Project_Model_Implementation.m
@@ -1,39 +1,250 @@
-%% Model Implementation
+%% New Test
 
-%%Parameter Values
-T_act = 0.01; %Activation constant time
-T_deact = 0.04; %Deactivation constant time
-J = 0.0197;
-d = 0.037;
-B = 0.82;
-cF = 0.1145;
-mF = 1.0275;
-av = 1.33;
-fv1 = 0.18;
-fv2 = 0.023;
-vmax = -0.9;
-Fmax = 600;
-W = 0.56;
-lT = 22.3;
-lMT_0 = 32.1;
-T_ela_param = [2.1, -0.08, -7.97, 0.19, 2.1];
+%% Plot
 
-x = [x1; x2; x3];
-dx = [dx1; dx2; dx3];
+tspan = [0 100];
 
-T_grav = zeros(1, length(t));
-T_acc = zeros(1, length(t));
-T_ela = zeros(1, length(t));
+% Initial conditions
 
-t = %sample time
-sys = ss(A, B, C, D, t);
+x0 = [1 1 1]; % Placeholder
 
-for i = 1:length(t)
-    T_grav(i) = B * c * (x1 - W);
-    T_acc(i) = mF * x3 * (lT - lMT_0);
-    T_ela(i) = T_ela_param(1) * (x1 - T_ela_param(2)) + T_ela_param(3) * (x1 - T_ela_param(4)) + T_ela_param(5);
+sol = ode45(@x_dot, tspan, x0);
 
-    dx1 = (u1 - x1) * ((u1 / T_act) - ((1 - u1)  /T_deact));
-    dx2 = x3;
-    dx3 = (1 / J) * (Fmax(i) * d - (fv1 * x3 + fv2 * x3^2) * x2 - (mF * x3 + a) * x3);
+x = sol.y;
+
+t = sol.x;
+
+figure(1)
+
+tiledlayout(3, 1);
+
+nexttile();
+
+plot(t, fact); hold on;
+
+ylabel({'Change in Dynamic';'Activation Level'; 'of Muscle'},'FontSize', 10); %fact_dot
+
+nexttile();
+
+plot(t, x(2,:)); hold on;
+
+ylabel({'Absolute Rotational'; 'Velocity of the Foot'},'FontSize',10) % alphaF_dot
+
+nexttile();
+
+plot(t, x(3,:)); hold on;
+
+ylabel({'Absolute Rotational'; 'Acceleration of the Foot'}, 'FontSize',10) % alphaF_dot_Dot
+
+xlabel('Time (ms)')
+
+%% Define x_dot(t,x)
+
+function x_dot = x_dot(t,x)
+
+% x1 = fact
+
+% x2 = alphaF
+
+% x3 = alphaF_dot
+
+fact = x(1);
+
+alphaF = x(2);
+
+alphaF_dot = x(3);
+
+% Input function
+
+% u = [u1] = [epsilon]
+
+u1 = input_func(t);
+
+% External States
+
+xext1 = xext1_func(t);
+
+xext2 = xext2_func(t);
+
+xext3 = xext3_func(t);
+
+xext4 = xext4_func(t);
+
+% make x_dot a column vector with 4 values then fill it appropriately.
+
+x_dot = [0; 0; 0];
+
+x_dot(1) = fact_dot(fact,u1);
+
+x_dot(2) = alphaF_dot;
+
+x_dot(3) = alphaF_dot_dot(fact, alphaF, alphaF_dot,u1, xext1, xext2, xext3, xext4);
+
 end
+
+%% Functions for input and external states wrt time (placeholders)
+
+function u1=input_func(t)
+
+u1 = 1*t ;
+
+end
+
+function xext1=xext1_func(t)
+
+xext1 = 1*t;
+
+end
+
+function xext2=xext2_func(t)
+
+xext2 = 1*t;
+
+end
+
+function xext3=xext3_func(t)
+
+xext3 = 1*t;
+
+end
+
+function xext4=xext4_func(t)
+
+xext4 = 1*t;
+
+end
+
+%% State Equations
+
+% x1_dot
+
+function fact_dot=fact_dot(fact,u1)
+
+% Constants
+
+Tact = 0.01;
+
+Tdeact = 0.04;
+
+fact_dot = (u1-fact)*((u1/Tact)/((1-u1)/Tdeact));
+
+end
+
+% x2_dot = alphaF_dot
+
+% x3_dot
+
+function alphaF_dot_dot = alphaF_dot_dot(fact, alphaF, alphaF_dot,u1, xext1, xext2, xext3, xext4)
+
+% Constants
+
+J = 0.0197;
+
+d = 3.7;
+
+B = 0.82;
+
+x = [fact,alphaF, alphaF_dot];
+
+alphaF_dot_dot = 1/J * (Fm(x,xext3,xext4)*d + Tgrav(alphaF)+Tacc(xext1,xext2,alphaF)+TEla(alphaF)+B*(xext4-alphaF_dot));
+
+end
+
+%% Other Equations
+
+function Fm=Fm(x,xext3, xext4)
+
+% Constants
+
+lT = 22.3;
+
+lMT0 = 32.1;
+
+d = 3.7;
+
+W = 0.56;
+
+av = 1.33;
+
+fv1 = 0.18;
+
+fv2 = 0.023;
+
+vmax = -0.9;
+
+Fmax = 600;
+
+vCE = d*(xext4 - x(3));
+
+lMT = lMT0 + d*(xext3-x(2));
+
+lCE = lMT - lT;
+
+lCEopt = 1; %can't figure out how to calculate this one!!!!
+
+% ffl(xext3 - x2)
+
+ffl_xext3_minus_x2 = exp(-((lCE-lCEopt)/(W*lCEopt))^2);
+
+% ffv(xext4 - x3)
+
+if (vCE < 0) % contraction
+
+   ffv_xext4_minus_x3 = (1-(vCE/vmax))/(1+(vCE/(vmax*fv1)));
+
+else % extension or isometric
+
+   ffv_xext4_minus_x3 = (1+av*(vCE/fv2))/(1+(vCE/fv2));
+
+end
+
+Fm = x(1)*Fmax*ffl_xext3_minus_x2*ffv_xext4_minus_x3;
+
+end
+
+function Tgrav=Tgrav(alphaF)
+
+% Constants
+
+cF = 11.45;
+
+mF = 1.0275;
+
+g = 9.81; % I made this one up
+
+Tgrav = -mF*cF*cos(alphaF)*g;
+
+end
+
+function Tacc=Tacc(xext1, xext2, alphaF)
+
+% Constants
+
+cF = 11.45;
+
+mF = 1.0275;
+
+Tacc = mF*cF*(xext1*sin(alphaF)-xext2*cos(alphaF));
+
+end
+
+function TEla = TEla(alphaF)
+
+% Constants
+
+a1 = 2.10;
+
+a2 = -0.08;
+
+a3 = -7.97;
+
+a4 = 0.19;
+
+a5 = -1.79;
+
+TEla = exp(a1+a2*alphaF)-exp(a3+a4*alphaF)+a5;
+
+end
+
+
+
---
+++ get_code_googledocs.py
@@ -0,0 +1,73 @@
+import os
+import io
+import re
+from googleapiclient.discovery import build
+from google.oauth2 import service_account
+from git import Repo
+
+# Google Docs API credentials
+SCOPES = ['https://www.googleapis.com/auth/documents.readonly']
+SERVICE_ACCOUNT_FILE = '/Users/ryanjing/matlab-pull-project-accd04b13ab0.json'  # Update with your credentials file
+
+# Git repository settings
+REPO_URL = 'https://github.com/Ryan-Jing/Foot-Drop-FES-Model.git'
+BRANCH = 'code-group-uploads'
+REPO_PATH = '/Users/ryanjing/MatLab_Projects/355_Project/Foot-Drop-FES-Model'  # Path to clone the repository
+FILE_PATH = 'BME_355_Project_Model_Implementation.m'
+
+def read_google_doc(service, doc_id):
+    doc = service.documents().get(documentId=doc_id).execute()
+    content = ''
+    for element in doc.get('body').get('content'):
+        if 'paragraph' in element:
+            paragraph = element['paragraph']
+            for elem in paragraph['elements']:
+                if 'textRun' in elem:
+                    text_run = elem['textRun']
+                    if 'content' in text_run:
+                        content += text_run['content']
+                content += '\n'  # Add a newline after each paragraph
+    return content
+
+def get_latest_commit_message(repo):
+    return repo.head.commit.message
+
+def main():
+    # Authenticate with Google Docs API
+    credentials = service_account.Credentials.from_service_account_file(SERVICE_ACCOUNT_FILE, scopes=SCOPES)
+    service = build('docs', 'v1', credentials=credentials)
+
+    # Connect to the repository
+    repo = Repo(REPO_PATH)
+    origin = repo.remote()
+
+    # Read the Google Docs file content
+    doc_id = '1afZ1KtkHvkHk6-m-ujB37t8B9DOTES-2cromKDjRjXs'
+    google_doc_content = read_google_doc(service, doc_id)
+
+    print(google_doc_content)
+
+    # Check if there are changes
+    latest_commit_message = get_latest_commit_message(repo)
+
+    # with open(os.path.join(REPO_PATH, FILE_PATH), 'r') as file:
+    #     local_file_content = file.read()
+
+    # if google_doc_content != local_file_content:
+        # Write the content to the file
+    with open(FILE_PATH, 'w') as file:
+        file.write(google_doc_content)
+
+    Stage and commit changes
+    repo.git.add(FILE_PATH)
+    repo.index.commit('Update from Google Docs')
+
+    # Push to the repository
+    origin.push(BRANCH)
+    print("Changes pushed to the repository.")
+
+    # else:
+    #     print("No changes detected.")
+
+if __name__ == '__main__':
+    main()
